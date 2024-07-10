1cbdc1323281f70ceeaed53f0b56f8262f215179
Fazliddinn7 <fazliddingadoyev034@gmail.com>
2024-07-10 23:25:35 +0500

    Add login and register views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from accounts.views import home_page
+from accounts.views import LoginView, logout_view, RegisterView
 
+app_name = 'accounts'
 urlpatterns = [
-    path('', home_page, name='home_page'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('signup/', RegisterView.as_view(), name='signup_page'),
 ]
--- hackogi/urls.py
+++ hackogi/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from accounts.views import home_page
+
+
 urlpatterns = [
+    path('', home_page, name='home_page'),
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     path('events/', include('events.urls')),
+
 ]
+
